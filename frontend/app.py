f0e621b6858fcf87d4d8e91ff3575a03dfe7ee50
Johann West <jwest@hagerty.com>
2024-09-24 15:37:19 -0700

    Boilderplate for frontend, backend, and docker container, run docker-compose up in root to run backend and frontend from docker

---
+++ frontend/app.py
@@ -0,0 +1,9 @@
+# frontend/app.py
+import streamlit as st
+
+def main():
+    st.title("GenAI Hackathon - Streamlit Frontend")
+    st.write("Frontend of our Generative AI project")
+
+if __name__ == "__main__":
+    main()
